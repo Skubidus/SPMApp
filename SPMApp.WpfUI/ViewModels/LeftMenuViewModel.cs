c5dca1e2b5968c0b2fcb543e555707aed981fa25
Skubidus <Skubidus@gmx.de>
2023-06-06 17:07:39 +0200

    Added UserControl for the left menu.

---
+++ SPMApp.WpfUI/ViewModels/LeftMenuViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SPMApp.WpfUI.ViewModels;
+
+public partial class LeftMenuViewModel : ObservableObject
+{
+
+}
---
+++ SPMApp.WpfUI/Views/LeftMenuView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using SPMApp.WpfUI.ViewModels;
+
+using System;
+using System.ComponentModel;
+using System.Configuration.Internal;
+using System.Windows.Controls;
+
+namespace SPMApp.WpfUI.Views
+{
+    /// <summary>
+    /// Interaction logic for LeftMenuView.xaml
+    /// </summary>
+    public partial class LeftMenuView : UserControl
+    {
+        private readonly IConfiguration _config;
+
+        public LeftMenuViewModel ViewModel { get; }
+
+        public LeftMenuView(IConfiguration config, LeftMenuViewModel viewModel)
+        {
+            _config = config;
+            ViewModel = viewModel;
+
+            DataContext = ViewModel;
+
+            InitializeComponent();
+        }
+
+        public LeftMenuView()
+        {
+            if (DesignerProperties.GetIsInDesignMode(this))
+            {
+                return;
+            }
+
+            _config = App.Services!.GetService<IConfiguration>()
+                ?? throw new InvalidOperationException("Configuration was null.");
+
+            ViewModel = App.Services!.GetService<LeftMenuViewModel>()
+                ?? throw new InvalidOperationException($"{nameof(ViewModel)} was null.");
+
+            DataContext = ViewModel;
+
+            InitializeComponent();
+        }
+    }
+}
