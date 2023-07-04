6a0693992579f61e970693cb8ca5060ab86b5f69
Skubidus <Skubidus@gmx.de>
2023-07-04 13:57:31 +0200

    Made tweaks to the ListBox that displays the entries. Also fixed the Binding (ItemsSource instead of DataContext).

--- SPMApp.WpfUI/ViewModels/EntryListViewModel.cs
+++ SPMApp.WpfUI/ViewModels/EntryListViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 using SPMLibrary.Data;
 using SPMLibrary.Models;
 
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace SPMApp.WpfUI.ViewModels;
 
@@ -14,11 +16,33 @@ public partial class EntryListViewModel : ObservableObject
     [ObservableProperty]
     private string _pageTitle = "Entry List";
 
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(SearchButtonClickCommand))]
+    private string? _searchText;
+
     private readonly ObservableCollection<Entry> _entries = new();
-    public ObservableCollection<Entry> Entrys => _entries;
+    public ObservableCollection<Entry> Entries => _entries;
 
     public EntryListViewModel(ISqLiteData db)
     {
         _db = db;
+        GetAllEntries();
+    }
+
+    public void GetAllEntries()
+    {
+        var entries = _db.GetAllEntries();
+
+        Entries.Clear();
+
+        entries.ForEach(e => { Entries.Add(e); });
+    }
+
+    private bool CanClickSearchButton => string.IsNullOrWhiteSpace(SearchText) == false && Entries.Any();
+
+    [RelayCommand(CanExecute = nameof(CanClickSearchButton))]
+    public void OnSearchButtonClick()
+    {
+
     }
 }
