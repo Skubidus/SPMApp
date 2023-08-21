c764879066843d5d8ae3f0e4b9ba4879ced89c79
Skubidus <Skubidus@gmx.de>
2023-08-21 15:52:14 +0200

    Added Equals() methods for EntryModel and TagModel.

--- SPMLibrary/Models/EntryModel.cs
+++ SPMLibrary/Models/EntryModel.cs
@@ -1,6 +1,5 @@
 ﻿namespace SPMLibrary.Models;
 
-//#nullable disable
 public class EntryModel
 {
     public int Id { get; set; }
@@ -12,5 +11,59 @@ public class EntryModel
     public List<TagModel> Tags { get; set; } = new();
     public DateTime DateCreated { get; set; }
     public DateTime DateModified { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not EntryModel
+            || obj is null)
+        {
+            return false;
+        }
+
+        var model = (EntryModel)obj;
+
+        if (model.Id != this.Id
+            || model.Title != this.Title
+            || model.Username != this.Username
+            || model.Password != this.Password
+            || model.WebsiteUrl != this.WebsiteUrl
+            || model.Notes != this.Notes
+            || AreTagListsEqual(model.Tags, this.Tags) == false
+            || model.DateCreated != this.DateCreated
+            || model.DateModified != this.DateModified)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool AreTagListsEqual(IEnumerable<TagModel> list1, IEnumerable<TagModel> list2)
+    {
+        var firstNotSecond = list1.Except(list2).ToList();
+        var secondNotFirst = list2.Except(list1).ToList();
+
+        bool output = (firstNotSecond.Any() == false)
+                   && (secondNotFirst.Any() == false);
+
+        return output;
+    }
+
+    public EntryModel Clone()
+    {
+        EntryModel output = new()
+        {
+            Id = this.Id,
+            Title = this.Title,
+            Username = this.Username,
+            Password = this.Password,
+            WebsiteUrl = this.WebsiteUrl,
+            Notes = this.Notes,
+            Tags = this.Tags.ToList(),
+            DateCreated = this.DateCreated,
+            DateModified = this.DateModified
+        };
+
+        return output;
+    }
 }
-//#nullable restore
--- SPMLibrary/Models/TagModel.cs
+++ SPMLibrary/Models/TagModel.cs
@@ -1,9 +1,28 @@
 ﻿namespace SPMLibrary.Models;
 
-#nullable disable
 public class TagModel
 {
     public int Id { get; set; }
+#nullable disable
     public string Title { get; set; }
-}
 #nullable restore
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not TagModel
+            || obj is null)
+        {
+            return false;
+        }
+        
+        var tag = obj as TagModel;
+
+        if (tag!.Id != this.Id
+            || tag.Title != this.Title)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
